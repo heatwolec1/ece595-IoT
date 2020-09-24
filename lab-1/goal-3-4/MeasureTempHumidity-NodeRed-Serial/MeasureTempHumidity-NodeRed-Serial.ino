681e6803ec2b26f0ed2b83f381b38d0bc9fb881f
Cory Heatwole <heatwolec1@udayton.edu>
2020-09-24 10:34:47 -0400

    Changed from symlink to actual file. Decided I want to make changes to it after all.

--- lab-1/goal-3-4/MeasureTempHumidity-NodeRed-Serial/MeasureTempHumidity-NodeRed-Serial.ino
+++ lab-1/goal-3-4/MeasureTempHumidity-NodeRed-Serial/MeasureTempHumidity-NodeRed-Serial.ino
@@ -1,1 +1,41 @@
-../../goal-1-2/MeasureTempHumidity/MeasureTempHumidity.ino
+/*
+ * This code is nearly identical to the example code provided by Adafruit for the
+ * SHTC3 sensor. My modications include printing the temperature in Fahrenheit
+ * and reformatting the text being printed to the serial monitor.
+*/
+
+#include "Adafruit_SHTC3.h"
+#include "Wire.h"
+
+Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
+
+float celsiusToFahrenheit(float tempC) {
+	return (tempC * 1.8) + 32;
+}
+
+void setup() {
+	Serial.begin(115200);
+
+	while (!Serial)
+		delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+	Serial.println("SHTC3 test");
+	if (! shtc3.begin()) {
+		Serial.println("Couldn't find SHTC3");
+		while (1) delay(1);
+	}
+	Serial.println("Found SHTC3 sensor");
+}
+
+void loop() {
+	sensors_event_t humidity, temp;
+
+	shtc3.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+
+	Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+	Serial.print("Temperature: "); Serial.print(celsiusToFahrenheit(temp.temperature)); Serial.println(" degrees F");
+	Serial.print("Humidity:    "); Serial.print(humidity.relative_humidity); Serial.println(" % rH");
+	Serial.println();	// Just to space out the measurements for reading at a glance
+
+	delay(1000);
+}
