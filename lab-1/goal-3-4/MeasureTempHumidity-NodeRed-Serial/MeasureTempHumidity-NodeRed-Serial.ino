880dc8b119f4bf973640f01c91aa04b54c72048a
Cory Heatwole <heatwolec1@udayton.edu>
2020-09-27 00:08:46 -0400

    Replaced long delays with millis time checks.

--- lab-1/goal-3-4/MeasureTempHumidity-NodeRed-Serial/MeasureTempHumidity-NodeRed-Serial.ino
+++ lab-1/goal-3-4/MeasureTempHumidity-NodeRed-Serial/MeasureTempHumidity-NodeRed-Serial.ino
@@ -13,34 +13,37 @@
 
 // Globals
 Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
+unsigned long prevMillis, curMillis;
 
 float celsiusToFahrenheit(float tempC) {
 	return (tempC * 1.8) + 32;
 }
 
 void setup() {
-	Serial.begin(115200);
+	prevMillis = 0;
 
+	Serial.begin(115200);
 	while (!Serial)
-		delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+		delay(10);     // 10 ms is short enough that delay function isn't problematic
 
 	Serial.println("SHTC3 test");
 	if (! shtc3.begin()) {
 		Serial.println("Couldn't find SHTC3");
-		while (1) delay(1);
+		while (1) delay(1);	// infinite loop, delay isn't stopping anything else from occurring
 	}
 	Serial.println("Found SHTC3 sensor");
 }
 
 void loop() {
-	sensors_event_t humidity, temp;
+	curMillis = millis();
+	if (curMillis-prevMillis >= 1000) {
+		sensors_event_t humidity, temp;
 
-	shtc3.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+		shtc3.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
 
-	Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
-	Serial.print("Temperature: "); Serial.print(celsiusToFahrenheit(temp.temperature)); Serial.println(" degrees F");
-	Serial.print("Humidity:    "); Serial.print(humidity.relative_humidity); Serial.println(" % rH");
-	Serial.println();	// Just to space out the measurements for reading at a glance
-
-	delay(1000);
+		Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+		Serial.print("Temperature: "); Serial.print(celsiusToFahrenheit(temp.temperature)); Serial.println(" degrees F");
+		Serial.print("Humidity:    "); Serial.print(humidity.relative_humidity); Serial.println(" % rH");
+		Serial.println();	// Just to space out the measurements for reading at a glance
+	}
 }
