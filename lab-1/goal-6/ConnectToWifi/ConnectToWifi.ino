2158d0a1d2a6bbd22c3176e0ce4c4b24c21a4446
Cory Heatwole <heatwolec1@udayton.edu>
2020-10-11 21:51:40 -0400

    Defined constants for wifi setup.

--- lab-1/goal-6/ConnectToWifi/ConnectToWifi.ino
+++ lab-1/goal-6/ConnectToWifi/ConnectToWifi.ino
@@ -1,5 +1,5 @@
 /* This is an example program for getting familiar with both the Wifi and the
- * SSD1306_OLED libraries. The ESP32 board will attempt to connect to a WiFi
+ * SSD1306_OLED libraries. The ESP32 board will attempt to connect to a Wifi
  * network (with a static IP address) and will print what it is doing and its
  * status messages to an SSD1306 OLED screen.
  */
@@ -9,6 +9,13 @@
 #include <SPI.h>
 #include <WiFi.h>
 #include <Wire.h>
+
+// Wifi constants
+#define NETWORK_SSID     "Cisco04646"
+#define NETWORK_PASSWORD "your_network_password"
+IPAddress local_IP(192, 168, 1, 140);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 void setup() {
 }
