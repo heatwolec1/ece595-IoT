222c4b7c3ae20e258e83fec172654628e8c7a80f
Cory Heatwole <heatwolec1@udayton.edu>
2020-10-11 21:40:50 -0400

    Started experimentation program for wifi and oled screen.

---
+++ lab-1/goal-6/ConnectToWifi/ConnectToWifi.ino
@@ -0,0 +1,12 @@
+/* This is an example program for getting familiar with both the Wifi and the
+ * SSD1306_OLED libraries. The ESP32 board will attempt to connect to a WiFi
+ * network (with a static IP address) and will print what it is doing and its
+ * status messages to an SSD1306 OLED screen.
+ */
+
+void setup() {
+}
+
+void loop() {
+}
+
